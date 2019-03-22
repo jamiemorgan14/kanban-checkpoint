d20544129f73f5ed3e05e302b315e37e4cc9ead6
Jamie <jmorgan3475@gmail.com>
2019-03-21 22:55:33 -0600

    improved login forms

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="login login-forms">
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 text-center">
         <h1>Welcome to KanBan</h1>
       </div>
     </div>
 
-    <div class="row">
-      <div class="form-container col-12 d-flex align-items-end justify-content-center">
-        <form v-if="loginForm" @submit.prevent="loginUser">
-          <input type="email" v-model="creds.email" placeholder="email" class="mr-4 login-form">
-          <input type="password" v-model="creds.password" placeholder="password" class="login-form">
+    <div class="row form-container" v-if="loginForm">
+      <div class="col-12 d-flex align-items-end justify-content-center">
+        <form @submit.prevent="loginUser" class="form-width">
+          <input type="email" v-model="creds.email" placeholder="eMail" class="login-form">
           <br>
-          <button type="submit" class="btn btn-outline-light mt-3 text-center">Login</button>
+          <input type="password" v-model="creds.password" placeholder="Password" class="login-form mt-1">
+          <br>
+          <button type="submit" class="btn btn-block btn-outline-light mt-3 text-center">Login</button>
         </form>
-        <form v-else @submit.prevent="register" class="mt-2 text-center">
-          <input type="text" v-model="newUser.name" placeholder="name" class="login-form mr-4">
-          <input type="email" v-model="newUser.email" placeholder="email" class="login-form">
-          <input type="password" v-model="newUser.password" placeholder="password" class="login-form ml-4">
+      </div>
+    </div>
+    <div v-if="!loginForm" class="row form-container">
+      <div class="col-6 text-center col-12 d-flex align-items-end justify-content-center">
+        <form @submit.prevent="register" class="mt-2 text-center form-width">
+          <input type="email" v-model="newUser.email" placeholder="eMail" class="login-form">
           <br>
-          <button type="submit" class="btn btn-outline-light mt-3 text-center">Create Account</button>
+          <input type="password" v-model="newUser.password" placeholder="Password" class="login-form mt-1">
+          <br>
+          <input type="text" v-model="newUser.name" placeholder="Username" class="login-form mt-1">
+          <br>
+          <button type="submit" class="btn btn-block btn-outline-light mt-3 text-center">Create Account</button>
         </form>
       </div>
     </div>
     <div class="row">
       <div class="col-12 form-container col-12 d-flex align-items-start justify-content-center"">
         <div class="
-        action mt-3" @click="loginForm = !loginForm">
+        action mt-3" @click="loginForm = !loginForm, newUser={}">
         <p v-if="loginForm">No account? Click here to Register</p>
         <p v-else>Already have an account? Click here to Login</p>
       </div>
@@ -39,6 +46,9 @@
   import router from "@/router.js";
   export default {
     name: "login",
+    mounted() {
+      this.creds.password = ''
+    },
     data() {
       return {
         loginForm: true,
@@ -71,16 +81,16 @@
 
   input {
     height: 50px;
-    width: 400px;
+    width: 100%;
     text-align: center;
     border: 1px solid rgba(224, 224, 224, 0.473);
   }
 
   .form-container {
-    height: 50vh
+    height: 40vh
   }
 
-  .note-container {
-    height: 40vh
+  .form-width {
+    width: 60%
   }
 </style>
